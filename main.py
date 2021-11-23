a923ebc35fee8b2ab134d20ead85990c6a4b4af1
Zack Hee Zy <zhiyang_0112@hotmail.co.uk>
2021-11-23 10:22:01 +0800

    test printing query

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import argparse
 import os
-import sys
+
+# from google.cloud import bigquery
+# from google.oauth2 import service_account
 
 environment = os.environ.get("ENVIRONMENT")
 
@@ -10,13 +12,23 @@ parser.add_argument("--filename", type=str)
 args = parser.parse_args()
 filename = args.filename
 
+# retrieve secret value her
 
-def hello(filename):
-    # with open(filename) as f:
-    #     sql = f.read().format(ENVIRONMENT=environment)
+
+def create_or_update_view(filename):
+    with open(filename) as f:
+        query = f.read().format(ENVIRONMENT=environment)
+
+    # credentials = service_account.Credentials.from_service_account_info(bq_credentials)
+    # bq_client = bigquery.Client(
+    #     credentials=credentials, project=f"data-{environment}-warehouse"
+    # )
+
+    # query_job = bq_client.query(query)
+    # query_job.result()
     # create view here
-    print(filename)
+    print(query)
 
 
 if __name__ == "__main__":
-    hello(str(filename))
+    create_or_update_view(str(filename))
--- sql-test/test.sql
+++ sql-test/test.sql
@@ -1,1 +1,33 @@
-data-{ENVIRONMENT}-warehouse
+CREATE OR REPLACE TABLE `data-{ENVIRONMENT}-warehouse.talent_acquisition.ct_job_app` CLUSTER BY company_id, job_id AS (
+SELECT  ja.company_id 
+        ,ja.job_id 
+        ,j.title                                      AS job_title 
+        ,j.role                                       AS job_role 
+        ,j.status                                     AS job_status 
+        ,ja.id                                        AS job_application_id 
+        ,ja.first_name 
+        ,ja.last_name 
+        ,ja.place_formatted_address 
+        ,ja.nationality 
+        ,ja.source 
+        ,ja.status 
+        ,ja.last_status_changed_at 
+        ,ja.is_questionnaires_required 
+        ,ja.is_video_required 
+        ,ja.total_questionnaires_completed 
+        ,ja.total_questionnaires 
+        ,ROUND(ja.assessment_completion_percentage,2) AS assessment_completion_percentage 
+        ,ja.assessment_completed_at 
+        ,ja.total_screenings_completed 
+        ,ja.total_screenings 
+        ,ROUND(ja.criteria_met_percentage,2)          AS criteria_met_percentage 
+        ,ja.has_passed_screening 
+        ,ja.role_fit_score                            AS role_fit_score 
+        ,ja.culture_fit_score                         AS culture_fit_score 
+        ,ja.submitted_at 
+        ,ja.completed_at 
+        ,ja.created_at 
+        ,ja.updated_at
+FROM `data-{ENVIRONMENT}-warehouse.talent_acquisition.job_application` ja
+LEFT JOIN `data-{ENVIRONMENT}-warehouse.talent_acquisition.job` j
+ON ja.job_id = j.id )
